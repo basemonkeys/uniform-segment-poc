6b23fea4067eca65c0ae91de95816961fc27bc34
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-09-15 10:00:25 -0700

    storybook update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
-import path from "path";
+import path, { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   framework: {
-    name: "@storybook/nextjs",
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
   stories: [
@@ -11,10 +11,10 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions"),
     {
       name: "@storybook/addon-styling",
       options: {
@@ -25,6 +25,7 @@ const config: StorybookConfig = {
         },
       },
     },
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
   docs: {
     autodocs: "tag",
@@ -40,3 +41,7 @@ const config: StorybookConfig = {
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
