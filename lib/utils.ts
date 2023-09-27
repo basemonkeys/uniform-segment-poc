53303d216be78f3a4766c074a404a2fa9abf476a
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-09-26 22:55:10 -0700

    cleans up utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // Get Image Url
-// TODO: remove this any type
 export function getImageUrl(image?: string | Types.CloudinaryImage | any) {
   const imageUrl = typeof image === "string" ? image : image?.[0]?.url;
 
@@ -16,11 +15,4 @@ export function getImageUrl(image?: string | Types.CloudinaryImage | any) {
   if (imageUrl.startsWith("//")) return imageUrl.replace("//", "https://");
 
   return imageUrl;
-}
-
-// Demo: https://res.cloudinary.com/demo/image/upload/w_300,c_limit,q_auto/turtles.jpg
-// TODO: remove this any type
-export function cloudinaryLoader({ src, width, quality }: any) {
-  const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
-  return `https://res.cloudinary.com/${params.join(",")}${src}`;
 }
