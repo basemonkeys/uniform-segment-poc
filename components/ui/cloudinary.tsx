3f4056b1cb7f423ab6c5d9e3893ac6b704993add
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-10-02 09:50:33 -0700

    removes custom types from cloudinary

--- components/ui/cloudinary.tsx
+++ components/ui/cloudinary.tsx
@@ -4,21 +4,6 @@ import { CldImage, CldVideoPlayer } from "next-cloudinary";
 
 import "next-cloudinary/dist/cld-video-player.css";
 
-// type CloudinaryImageProps = {
-//   src: string;
-//   width: number;
-//   height?: number;
-//   // fill: boolean;
-//   alt: string;
-//   className?: string;
-// };
-
-type CloudinaryVideoProps = {
-  width: string | number;
-  height: string | number;
-  src: string;
-};
-
 export const CloudinaryImage = (props: any) => {
   return <CldImage {...props} />;
 };
