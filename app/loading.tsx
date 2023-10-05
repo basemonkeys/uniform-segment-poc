cabd84d15c432b751c5d3cfbcf592214e10344ba
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-10-05 09:05:08 -0700

    adds SS logo to loading.js

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,19 @@
 // This is a site wide loading state and can be overriden for component specific loading states with React <Suspense />
 
+import { CloudinaryImage } from "@/components/ui/cloudinary";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Loading() {
   return (
-    <main className="flex h-screen items-center justify-center">
+    <main className="flex h-screen flex-col items-center justify-center space-y-6">
+      <CloudinaryImage
+        src="silversneakers/logo_primary_vk0ca4"
+        alt="Loading"
+        width={250}
+        height={100}
+      ></CloudinaryImage>
       <h3 className="flex animate-pulse items-center text-primary">
         <FontAwesomeIcon
           icon={faCircleNotch}
