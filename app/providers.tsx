a22484db6bcd5a90304cf178db686da4e3ea6d96
Seth Hall <seth.hall@tivityhealth.com>
2023-10-11 17:35:35 -0700

    adds SDK, route, and UserProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <UserProvider>{children}</UserProvider>
+    </div>
+  );
 }
