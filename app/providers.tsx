85b388f743565ecab28a47d5ae07f863f8d70a96
Seth Hall <seth.hall@tivityhealth.com>
2023-10-25 12:05:37 -0700

    basic Jotai integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,12 @@
+// import { Provider as UIProvider } from "jotai";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      {/* <UIProvider> */}
       <UserProvider>{children}</UserProvider>
+      {/* </UIProvider> */}
     </div>
   );
 }
