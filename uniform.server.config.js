d3328adcdcfba0bdceafb55c7484827385f29a7b
mslabyak <mslabyak@basemonkeys.com>
2023-11-20 20:31:30 -0500

    try again

--- uniform.server.config.js
+++ uniform.server.config.js
@@ -1,4 +1,9 @@
 /** @type {import('@uniformdev/canvas-next-rsc/config').UniformServerConfig} */
 module.exports = {
   defaultConsent: true,
+  plugins: [enableContextDevTools()],
+  evaluation: {
+    personalization: 'client',
+    testing: 'client'
+  }
 };
