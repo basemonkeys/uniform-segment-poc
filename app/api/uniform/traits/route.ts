6e68111e60839a91db424283d3d4f9c945d6ea48
mslabyak <mslabyak@basemonkeys.com>
2023-11-22 13:13:37 -0500

    update route to use NextRequest

--- app/api/uniform/traits/route.ts
+++ app/api/uniform/traits/route.ts
@@ -1,34 +1,38 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import {  NextResponse } from 'next/server';
 import axios from 'axios';
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 
-export  async function GET(req: NextApiRequest, res: NextApiResponse): Promise<NextResponse> {
-
-  
-
+export async function GET(): Promise<NextResponse> {
   try {
     const segmentSpaceId = process.env.SEGMENT_SPACE_ID!;
     const segmentApiKey = process.env.SEGMENT_API_KEY!;
     
-    const cookieStore = cookies()
+    const cookieStore = cookies();
     const ajs_anonymous_id = cookieStore.get('ajs_anonymous_id')?.value || '';
 
     const url = `https://profiles.segment.com/v1/spaces/${segmentSpaceId}/collections/users/profiles/anonymous_id:${ajs_anonymous_id}/traits`;
     const basicAuth = Buffer.from(segmentApiKey + ':').toString('base64');
-    //console.error('url', url);
+
     const response = await axios.get(url, {
       headers: {
         Authorization: `Basic ${basicAuth}`
       },
     });
 
-    return NextResponse.json(response.data);
-  } catch (error: any) {
+    return NextResponse.json(response.data, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  } catch (error) {
     return NextResponse.json({
-      error: error.message || 'Internal Server Error'
+      error: 'Internal Server Error'
     }, {
-      status: 500 // Set the status code to 500
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
   }
 }
