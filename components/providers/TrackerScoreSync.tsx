0c1d2e71ef87841d0b9c5a6179cbaec04c768669
mslabyak <mslabyak@basemonkeys.com>
2023-11-22 12:43:02 -0500

    update segment tracker

--- components/providers/TrackerScoreSync.tsx
+++ components/providers/TrackerScoreSync.tsx
@@ -2,13 +2,16 @@
 import { useUniformContext } from '@uniformdev/canvas-next-rsc/component';
 import { useEffect } from 'react';
 
-
 export const TrackerScoreSync = () => {
   const { context } = useUniformContext();
+  //console.log("you are in the tracker sync");
+  //console.log(fetch('/api/uniform/traits'));
   useEffect(() => {
     const fetchTraits = async () => {
+      console.log("calling fetchTraits");
       const response = await fetch('/api/uniform/traits');
       const { traits } = await response.json();
+    
       await context?.update({
         quirks: {
           ...traits,
